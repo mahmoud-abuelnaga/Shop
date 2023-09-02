ba9934b0211349d5537a7851c3c8dcfeb1029cbb
Mahmoud Abuelnaga <abuelnaga.m0@gmail.com>
2023-09-02 14:45:58 +0300

    Enabled downloading invoices for products

--- routes/shop.js
+++ routes/shop.js
@@ -1,30 +1,37 @@
 // Utilites
-const path = require('path');
+
 
 // NPM Packages
-const express = require('express');
+const express = require("express");
 
 // Controllers
-const shopController = require('../controllers/shop');
-const {isLoggedIn, verifyToken} = require('../middlewares/auth');
+const shopController = require("../controllers/shop");
+const { isLoggedIn, verifyToken } = require("../middlewares/auth");
 
 // Routes
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+router.get("/", shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
-router.get('/products/:id', shopController.getProductDetails);  // Adding dynamic route
+router.get("/products/:id", shopController.getProductDetails); // Adding dynamic route
 
-router.get('/cart', isLoggedIn, shopController.getCart);
+router.get("/cart", isLoggedIn, shopController.getCart);
 
-router.post('/cart', isLoggedIn, verifyToken, shopController.updateCart)
+router.post("/cart", isLoggedIn, verifyToken, shopController.updateCart);
 
-router.get('/orders', isLoggedIn, shopController.getOrders);
+router.get("/orders", isLoggedIn, shopController.getOrders);
 
-router.post('/create-order', isLoggedIn, verifyToken, shopController.createOrder);
+router.post(
+    "/create-order",
+    isLoggedIn,
+    verifyToken,
+    shopController.createOrder
+);
 
-router.get('/checkout', isLoggedIn, shopController.getCheckout);
+router.get("/checkout", isLoggedIn, shopController.getCheckout);
+
+router.get("/invoice/:orderId", isLoggedIn, shopController.getInvoice);
 
 module.exports = router;
