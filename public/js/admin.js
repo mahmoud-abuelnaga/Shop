600d4836b5d03080ec8097b81c1e6db4790b3892
Mahmoud Abuelnaga <abuelnaga.m0@gmail.com>
2023-09-16 06:41:47 +0300

    Fixed delete product bug introduced from setting Content-Security-Policy secure header

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,47 +2,51 @@
 const main = document.querySelector("main");
 const grid = document.querySelector(".grid");
 const pagination = document.querySelector(".pagination");
+const deleteButtons = document.querySelectorAll(".deleteBtn");
 
-/**
- *
- * @param {HTMLButtonElement} btn
- */
-const deleteProduct = async (btn) => {
-    const token = btn.parentElement.querySelector("input[name='token']").value;
-    const productId = btn.parentElement.querySelector(
-        "input[name='productId']"
-    ).value;
+console.log(deleteButtons);
+for (let btn of deleteButtons) {
+    btn.addEventListener("click", async () => {
+        const token = btn.parentElement.querySelector(
+            "input[name='token']"
+        ).value;
+        const productId = btn.parentElement.querySelector(
+            "input[name='productId']"
+        ).value;
 
-    const response = await fetch(`/admin/products/${productId}`, {
-        method: "DELETE",
-        headers: {
-            token,
-        },
-    });
+        const response = await fetch(`/admin/products/${productId}`, {
+            method: "DELETE",
+            headers: {
+                token,
+            },
+        });
 
-    if (response.status == 200) {
-        const productArticle = btn.closest("article");
-        productArticle.remove();
+        if (response.status == 200) {
+            const productArticle = btn.closest("article");
+            productArticle.remove();
 
-        const topArticle = grid.querySelector(".card.product-item");
-        if (!topArticle) {
-            const urlParams = new URLSearchParams(window.location.search);
-            const page = parseInt(urlParams.get('page'));
-            
-            if (!page || page == 1) {
-                grid.remove();
-                pagination.remove();
-                
-                const h1 = document.createElement("h1");
-                h1.appendChild(document.createTextNode("No Products Found!"));
-                main.appendChild(h1);
-            } else {
-                window.location.replace(`/admin/products?page=${page - 1}`);
+            const topArticle = grid.querySelector(".card.product-item");
+            if (!topArticle) {
+                const urlParams = new URLSearchParams(window.location.search);
+                const page = parseInt(urlParams.get("page"));
+
+                if (!page || page == 1) {
+                    grid.remove();
+                    pagination.remove();
+
+                    const h1 = document.createElement("h1");
+                    h1.appendChild(
+                        document.createTextNode("No Products Found!")
+                    );
+                    main.appendChild(h1);
+                } else {
+                    window.location.replace(`/admin/products?page=${page - 1}`);
+                }
             }
+        } else if (response.status == 500) {
+            window.location.replace("/500");
+        } else if (response.status == 403) {
+            window.location.replace("/authentication-error");
         }
-    } else if (response.status == 500) {
-        window.location.replace("/500");
-    } else if (response.status == 403) {
-        window.location.replace("/authentication-error");
-    }
-};
+    });
+}
